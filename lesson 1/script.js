713f2f3ee0272bffdcb54a80ed76b99759394586
Suyun <ice-fucking@mail.ru>
2019-04-24 15:59:08 +0300

    это наш первый комит

---
+++ lesson 1/script.js
@@ -0,0 +1,28 @@
+`use strict`;
+//alert()
+
+let something;
+console.log(something);
+
+let persone = {
+    name: "John",
+    age: 25,
+    isMarried: false
+};
+
+console.log(persone["name"]);
+
+
+let arr = ['plum.png','orange.jpg', 'apple.bmp'];
+
+console.log(arr[0])
+
+//alert("hello world");
+
+// let answer = confirm("you are here?");
+
+// console.log(answer);
+
+let answer = prompt("есть ли вм 18?", "да");
+
+console.log(answer); 
